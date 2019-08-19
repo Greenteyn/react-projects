295e9706e6beea9f73d0c0f01bd4fd150cb1c74d
Greenteyn <e.polyakov@qlean.ru>
2019-08-19 14:44:05 +0300

    complection conponent project

--- components/src/index.js
+++ components/src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-        <CommentDetail author="Jane" timeAgo="Сегодня в 12:43" content={faker.lorem.words()} avatar={faker.image.avatar()} />
-        <CommentDetail author="Eugene" timeAgo="Сегодня в 14:40" content={faker.lorem.words()} avatar={faker.image.avatar()} />
-        <CommentDetail author="Drake" timeAgo="Вчера в 19:30" content={faker.lorem.words()} avatar={faker.image.avatar()} />
-        </div>
-    );
+	return (
+  	<div className="ui container comments">
+			<ApprovalCard>
+    		<CommentDetail author="Jane" timeAgo="Сегодня в 12:43" content={faker.lorem.words()} avatar={faker.image.avatar()} />
+			</ApprovalCard>
+			<ApprovalCard>
+      	<CommentDetail author="Eugene" timeAgo="Сегодня в 14:40" content={faker.lorem.words()} avatar={faker.image.avatar()} />
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail author="Drake" timeAgo="Вчера в 19:30" content={faker.lorem.words()} avatar={faker.image.avatar()} />
+			</ApprovalCard>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
